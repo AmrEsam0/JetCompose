0688e1e869c62b581008b7cda5f48548ef679151
AmrEssam <amr_esam@ymail.com>
2023-01-07 03:21:51 +0200

    Basic animation with cards and text

--- app/src/main/java/com/example/tasky2/MainActivity.kt
+++ app/src/main/java/com/example/tasky2/MainActivity.kt
@@ -1,14 +1,32 @@
 package com.example.tasky2
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tasky2.ui.theme.Tasky2Theme
@@ -18,40 +36,138 @@ class MainActivity : ComponentActivity() {
 		super.onCreate(savedInstanceState)
 		setContent {
 			Tasky2Theme {
-				MyApp()
-				}
+				MyApp(modifier = Modifier.fillMaxSize())
 			}
 		}
 	}
-@Preview(showBackground = true)
+}
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+	var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+	Surface(modifier, color = MaterialTheme.colors.background) {
+		if (shouldShowOnboarding) {
+			OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+		} else {
+			Greetings()
+		}
+	}
+}
+
+@Composable
+fun OnboardingScreen(
+	onContinueClicked: () -> Unit,
+	modifier: Modifier = Modifier
+) {
+	Column(
+		modifier = modifier.fillMaxSize(),
+		verticalArrangement = Arrangement.Center,
+		horizontalAlignment = Alignment.CenterHorizontally
+	) {
+		Text("Welcome to the Basics Codelab!")
+		Button(
+			modifier = Modifier.padding(vertical = 24.dp),
+			onClick = onContinueClicked
+		) {
+			Text("Continue")
+		}
+	}
+}
+
+@Composable
+private fun Greetings(
+	modifier: Modifier = Modifier,
+	names: List<String> = List(10) { "$it" }
+) {
+	LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+		items(items = names) { name ->
+			Greeting(name = name)
+		}
+	}
+}
+
+@Composable
+private fun Greeting(name: String) {
+	Card(
+		backgroundColor = MaterialTheme.colors.primary,
+		modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+	) {
+		CardContent(name)
+	}
+}
+
+@Composable
+private fun CardContent(name: String) {
+	var expanded by remember { mutableStateOf(false) }
+
+	Row(
+		modifier = Modifier
+			.padding(12.dp)
+			.animateContentSize(
+				animationSpec = spring(
+					dampingRatio = Spring.DampingRatioMediumBouncy,
+					stiffness = Spring.StiffnessLow
+				)
+			)
+	) {
+		Column(
+			modifier = Modifier
+				.weight(1f)
+				.padding(12.dp)
+		) {
+			Text(text = "Hello, ")
+			Text(
+				text = name, style = MaterialTheme.typography.h4.copy(
+					fontWeight = FontWeight.ExtraBold
+				)
+			)
+			if (expanded) {
+				Text(
+					text = ("Composem ipsum color sit lazy, " +
+							"padding theme elit, sed do bouncy. ").repeat(4),
+				)
+			}
+		}
+		IconButton(onClick = { expanded = !expanded }) {
+			Icon(
+				imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+				contentDescription = if (expanded) {
+					"asdasd"
+				} else {
+					"asdasdasdasdasdasd"
+				}
+			)
+		}
+	}
+}
+
+@Preview(
+	showBackground = true,
+	widthDp = 320,
+	uiMode = UI_MODE_NIGHT_YES,
+	name = "DefaultPreviewDark"
+)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
 	Tasky2Theme {
-		Greeting("Android")
+		Greetings()
 	}
 }
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
-fun Greeting(name: String) {
-	Surface(
-		color = MaterialTheme.colors.primary,
-		modifier = Modifier.padding(horizontal = 8.dp, vertical = 8.dp)) {
-		Column(modifier = Modifier.padding(24.dp).fillMaxWidth(1f)) {
-			Text(text = "Hello, ")
-			Text(text = name)
-		}
+fun OnboardingPreview() {
+	Tasky2Theme {
+		OnboardingScreen(onContinueClicked = {})
 	}
 }
 
+@Preview
 @Composable
-fun MyApp(names: List<String> = listOf("Dalia", "Dareen", "Lina")) {
-	// A surface container using the 'background' color from the theme
-		Surface(color = MaterialTheme.colors.background) {
-		Column {
-			for (name in names) {
-				Greeting(name)
-			}
-		}
+fun MyAppPreview() {
+	Tasky2Theme {
+		MyApp(Modifier.fillMaxSize())
 	}
 }
-
-
